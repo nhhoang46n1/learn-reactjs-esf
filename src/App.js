f765db2a0c7b70f05a32c6fc1e7b4c8ea3dbcb7f
Nguyen Huy Hoang <nhhoang46n1@gmail.com>
2021-11-09 23:04:27 +0700

    update 2

--- src/App.js
+++ src/App.js
@@ -9,14 +9,9 @@ function App() {
         <p>
           Hoang Nguyen
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
           Learn React
-        </a>
+        
       </header>
     </div>
   );
